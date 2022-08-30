8b15eac09dfaaf6ebca3844cd9495b77266e2b2c
SANOX993 <danilyatsiuk@gmail.com>
2022-08-30 13:47:41 +0300

    'commit3'

--- send.php
+++ send.php
@@ -1,20 +1,32 @@
-<?php 
-    $fio = $_POST['fio'];
-    $email = $_POST['email'];
-    $fio = htmlspecialchars($fio);
-    $email = htmlspecialchars($email);
-    $fio = urldecode($fio);
-    $email = urldecode($email);
-    $fio = trim($fio);
-    $email = trim($email);
-    //echo $fio;
-    //echo "<br>";
-    //echo $email;
-    mail("danitsiuk@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: dantsiuk@gmail.com \r\n");
-    if (mail("danitsiuk@gmail.com", "Заказ с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: dantsiuk@gmail.com \r\n"))
- {
-    echo "сообщение успешно отправлено";
+<html>
+<head>
+<title>Форма заявки с сайта</title>
+</head>
+<body>
+<?php
+//проверяем, существуют ли переменные в массиве POST
+if(!isset($_POST['fio']) and !isset($_POST['email'])){
+ ?> <form action="send.php" method="post">
+<input type="text" name="fio" placeholder="Укажите ФИО" required>
+<input type="text" name="email" placeholder="Укажите e-mail" required>
+<input type="submit" value="Отправить">
+</form> <?php
 } else {
-    echo "при отправке сообщения возникли ошибки";
+ //показываем форму
+ $fio = $_POST['fio'];
+ $email = $_POST['email'];
+ $fio = htmlspecialchars($fio);
+ $email = htmlspecialchars($email);
+ $fio = urldecode($fio);
+ $email = urldecode($email);
+ $fio = trim($fio);
+ $email = trim($email);
+ if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n")){
+ echo "Сообщение успешно отправлено";
+ } else {
+ echo "При отправке сообщения возникли ошибки";
+ }
 }
 ?>
+</body>
+</html>
